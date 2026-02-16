2e6b22e94b6110931c7d9f1525412d8d152fd35e
MD IRFAN <141354177+Irfan4545@users.noreply.github.com>
2026-02-16 22:25:37 +0530

    Add files via upload

---
+++ Assignment1.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    float sum = 0, average = 0, greaterSum = 0;
+
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    printf("Enter %d elements:\n", n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+        sum += arr[i];
+    }
+
+    average = sum / n;
+
+    for (int i = 0; i < n; i++) {
+        if (arr[i] > average) {
+            greaterSum += arr[i];
+        }
+    }
+
+    printf("Average of the array = %.2f\n", average);
+    printf("Sum of elements greater than the average = %.2f\n", greaterSum);
+
+    return 0;
+}
